f3897720ef7f8f4fe20dd4b314e498c5182dc274
L3GEND-AI <playstation74@list.ru>
2024-05-12 19:52:18 +0300

    Added login/logout function

--- crmBeautyStudio/website/views.py
+++ crmBeautyStudio/website/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
+    context: dict[str, str] = {"title": "Я&Ты - Главная"}
     return render(request, "website/home.html")
+
+
+def loginuser(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Вы успешно вошли в систему")
+            return redirect("home")
+        else:
+            messages.error(request, "Возникла ошибка при входе. Проверьте введенные данные.")
+    else:
+        return render(request, "website/login.html")
+
+
+def logoutuser(request):
+    logout(request)
+    messages.success(request, "Вы успешно вышли из системы.")
+    return redirect("home")
