71d5aa282c8f236c945b83a39203cd63bd94f963
L3GEND-AI <playstation74@list.ru>
2024-05-20 13:45:31 +0300

    clientsList, userData functions added

--- crmBeautyStudio/website/views.py
+++ crmBeautyStudio/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import User
 
 
 def home(request):
@@ -29,3 +30,15 @@ def logoutuser(request):
     logout(request)
     messages.success(request, "Вы успешно вышли из системы.")
     return redirect("home")
+
+def clientsList(request):
+    users = User.objects.all()
+    return render(request, "website/clients.html", {"users": users})
+
+def userdata(request, pk):
+    if request.user.is_authenticated:
+        user = User.objects.get(id=pk)
+        return render(request, "website/userdata.html", {"user": user})
+    else:
+        messages.error(request, "Вы должны авторизоваться для просмотра данной страницы")
+        return redirect("home")
