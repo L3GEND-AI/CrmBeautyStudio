a23d77bedfa36d2a1c821b754578f18bf4b3ff7f
L3GEND-AI <playstation74@list.ru>
2024-05-21 06:34:48 +0300

    config

--- crmBeautyStudio/crmBeautyStudio/settings.py
+++ crmBeautyStudio/crmBeautyStudio/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'crmBeautyStudio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS: list[Path] = [
     BASE_DIR / 'static'
     ]
