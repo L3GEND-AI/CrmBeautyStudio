00e70db6c1080533b721cd1e5883e5681976deaa
L3GEND-AI <playstation74@list.ru>
2024-05-26 02:10:52 +0300

    Added form for Service edit

---
+++ crmBeautyStudio/website/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Services
+
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model = Services
+        fields = ['name_service', 'price', 'category_service', 'description', 'slug']
+        widgets = {
+            'name_service': forms.TextInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'category_service': forms.Select(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+        }
