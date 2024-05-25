44852e682dcdc46ef056e66a5114eb114e6df708
L3GEND-AI <playstation74@list.ru>
2024-05-26 02:11:14 +0300

    Urls for services added

--- crmBeautyStudio/website/urls.py
+++ crmBeautyStudio/website/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('logout/', views.logoutuser, name='logout'),
     path('clients/', views.clientsList, name='clients'),
     path('services/', views.servicesList, name='services'),
-    #path('profile/', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('clients/record/<int:pk>/', views.record, name='record'),
+    path('services/service_detail/<int:pk>/', views.service_detail, name='service_detail'),
+    path('service/<int:pk>/edit/', views.edit_service, name='edit_service'),
 ]
