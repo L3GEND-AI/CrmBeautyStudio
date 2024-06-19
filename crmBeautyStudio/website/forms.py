da1dd4a7bf300a7b318d73470b80088e9f5f153f
L3GEND-AI <playstation74@list.ru>
2024-06-19 14:27:35 +0300

    Userform, Blognews form added

--- crmBeautyStudio/website/forms.py
+++ crmBeautyStudio/website/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Services
+from .models import Services, Blognews, User
 
 class ServiceForm(forms.ModelForm):
     class Meta:
@@ -11,4 +11,26 @@ class ServiceForm(forms.ModelForm):
             'category_service': forms.Select(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
             'slug': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+class BlognewsForm(forms.ModelForm):
+    class Meta:
+        model = Blognews
+        fields = ['tittle', 'description', 'image', 'maintext']
+        widgets = {
+            'tittle': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите заголовок'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Введите краткое описание'}),
+            'image': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите URL изображения'}),
+            'maintext': forms.Textarea(attrs={'class': 'form-control', 'rows': 10, 'placeholder': 'Введите основной текст'}),
+        }
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'PhoneNumber']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'PhoneNumber': forms.TextInput(attrs={'class': 'form-control'}),
         }
